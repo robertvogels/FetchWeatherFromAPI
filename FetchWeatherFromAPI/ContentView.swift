007ea849a0d87e5abc58da51ea78fde8db09568a
Robert Vogels <robertvogels@gmx.com>
2019-11-06 12:23:06 +0100

    Initial Commit

---
+++ FetchWeatherFromAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FetchWeatherFromAPI
+//
+//  Created by Robert on 06/11/2019.
+//  Copyright © 2019 Robert Vogels. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
